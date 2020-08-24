5ae42f6a05b42b940f10aac948effd3bbf0422f6
Keith <taylor.km61@icloud.com>
2020-08-24 15:55:44 +0100

    Method stubbing I

---
+++ README.md
@@ -0,0 +1,22 @@
+## Five types of Test doubles
+- Dummy
+ 
+  Mainly used to fill in parameter lists
+  
+- Fake
+
+  A lightweight example of an object that the unit under test requires - e.g. inmemory DB
+  
+- Stub
+
+  An object that only writes pre-programmed answers - naive implementation
+  
+- Spy
+
+  Like a stub but with the capability to remember how it was called
+  
+- Mock
+
+  Configured with expectations which are a description of the calls they expect to recieve during the test
+  Only mocks insist in behaviour testing
+ 
--- src/test/java/com/pluralsight/pension/setup/AccountOpeningServiceTest.java
+++ src/test/java/com/pluralsight/pension/setup/AccountOpeningServiceTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class AccountOpeningServiceTest {
 
+    private static final String UNACCEPTABLE_RISK_PROFILE = "HIGH";
     private AccountOpeningService underTest;
     // Call static mock method for all three collaborators
     private BackgroundCheckService backgroundCheckService = mock(BackgroundCheckService.class);
@@ -28,5 +30,16 @@ class AccountOpeningServiceTest {
     public void shouldOpenAccount() throws IOException {
         final AccountOpeningStatus accountOpeningStatus = underTest.openAccount("John","Smith","123xyz9", LocalDate.of(1990,1,1));
         assertEquals(AccountOpeningStatus.OPENED,accountOpeningStatus);
+    }
+
+    @Test
+    public void shouldDeclineAccount() throws IOException {
+        // Use when to return expected values from mock.
+        // Mockito always returns default values for unstubbed methods, null for reference types
+        // In this case it didn't matter whether we stubbed backgrounCheckService or not
+        when(backgroundCheckService.confirm("John","Smith","123", LocalDate.of(1990,1,1)))
+                .thenReturn(new BackgroundCheckResults(UNACCEPTABLE_RISK_PROFILE, 0));
+        final AccountOpeningStatus accountOpeningStatus = underTest.openAccount("John","Smith","123", LocalDate.of(1990,1,1));
+        assertEquals(AccountOpeningStatus.DECLINED,accountOpeningStatus);
     }
 }
